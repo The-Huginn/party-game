a6301ee582856382b89e0fa475d06a68f1c06c53
bifrost <bifrost@1976766399.kn.vutbr.cz>
2023-04-23 17:06:43 +0200

    Add builder script for updating to new versions.

---
+++ build.sh
@@ -0,0 +1,14 @@
+. ~/.configs/registry.config
+
+git pull origin
+cd app && \
+podman build -t registry.localhost/game:latest .
+
+podman login -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} registry.localhost
+podman push registry.localhost/game:latest
+
+kubectl scale --replicas=0 deploy/nginx
+kubectl scale --replicas=0 deploy/backend
+
+kubectl scale --replicas=1 deploy/backend
+kubectl scale --replicas=1 deploy/nginx
