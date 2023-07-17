a3f0fa03050d58229cb1a56169c20a830cc87c34
bifrost <bifrost@thehuginn.com>
2023-07-17 10:25:06 +0200

    Update building script to deploy game-service

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 . ~/.configs/registry.config
 
 git pull origin
-cd src/app && \
-podman build -t registry.localhost/game:latest .
+cd backend/game-service && \
+mvn clean install -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.resolver.transport=wagon && \
+sed -i 's/thehuginn.com/localhost/g' target/kubernetes/kubernetes.yml && \
+kubectl apply -f target/kubernetes/kubernetes.yml
 
-podman login -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} registry.localhost
-podman push registry.localhost/game:latest
+#podman login -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} registry.localhost
+#podman push registry.localhost/game:latest
 
-kubectl scale --replicas=0 deploy/nginx
-kubectl scale --replicas=0 deploy/backend
+#kubectl scale --replicas=0 deploy/nginx
+#kubectl scale --replicas=0 deploy/backend
 
-kubectl scale --replicas=1 deploy/backend
-kubectl scale --replicas=1 deploy/nginx
+#kubectl scale --replicas=1 deploy/backend
+#kubectl scale --replicas=1 deploy/nginx
