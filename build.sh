284640aae68b1c250cc7e33776c5528c399d92cb
bifrost <bifrost@thehuginn.com>
2023-07-18 01:35:09 +0200

    Update build.sh to include task service

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 . ~/.configs/registry.config
 
 git pull origin
-cd backend/game-service && \
-mvn clean install -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.resolver.transport=wagon && \
-sed -i 's/thehuginn.com/localhost/g' target/kubernetes/kubernetes.yml && \
-kubectl apply -f target/kubernetes/kubernetes.yml
+
+for microservice in game-service task-game
+do
+	cd backend/$microservice&& \
+	mvn clean install -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.resolver.transport=wagon && \
+	sed -i 's/thehuginn.com/localhost/g' target/kubernetes/kubernetes.yml && \
+	kubectl apply -f target/kubernetes/kubernetes.yml
+done
 
 #podman login -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} registry.localhost
 #podman push registry.localhost/game:latest
