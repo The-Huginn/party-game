5e113e3e75e3e6b7ebfc8df14c3133969d3aba16
bifrost <bifrost@thehuginn.com>
2023-07-18 02:38:54 +0200

    Update building script and ingress to point to svelte frontend

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@ git pull origin
 
 for microservice in game-service task-game
 do
-	cd backend/$microservice&& \
-	mvn clean install -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.resolver.transport=wagon && \
+	cd ~/party-game/backend/$microservice&& \
+	mvn clean install -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.resolver.transport=wagon -DskipTests && \
 	sed -i 's/thehuginn.com/localhost/g' target/kubernetes/kubernetes.yml && \
 	kubectl apply -f target/kubernetes/kubernetes.yml
 done
+
+kubectl apply -f ~/party-game/frontend/frontend.yaml
 
 #podman login -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} registry.localhost
 #podman push registry.localhost/game:latest
