562fb00821c56e9c3423c0859fc7569588aeff66
The-Huginn <rbudinsk@redhat.com>
2023-08-13 16:48:01 +0200

    Add equals and hashCode method to Player.

--- src/game-service/src/main/java/com/thehuginn/entities/Player.java
+++ src/game-service/src/main/java/com/thehuginn/entities/Player.java
@@ -3,7 +3,22 @@ package com.thehuginn.entities;
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 
+import java.util.Objects;
+
 @Entity
 public class Player extends PanacheEntity {
     String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
