9f62b2f6e52618d3c04b82198e7c5ba7a3ea3aad
The-Huginn <rbudinsk@redhat.com>
2023-09-09 12:31:06 +0200

    Remove python related files and add temporary script for generating tasks.

--- mongo.sh
+++
@@ -1,1 +0,0 @@
-podman run -it --name mongo --env MONGO_INITDB_ROOT_USERNAME=admin --env MONGO_INITDB_ROOT_PASSWORD=admin -p 127.0.0.1:27017:27017 mongo:latest
---
+++ populate-tasks.sh
@@ -0,0 +1,125 @@
+curl -X 'POST' \
+  'http://localhost:8082/task' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "type": "SINGLE",
+  "repeat": "ALWAYS",
+  "frequency": 1,
+  "price": {
+    "enabled": true,
+    "price": 0
+  },
+  "task": {
+    "locale": "en",
+    "content": "{player_c} talk to {player_1} for {timer_10}"
+  }
+}'
+
+curl -X 'POST' \
+  'http://localhost:8082/task' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "type": "SINGLE",
+  "repeat": "ALWAYS",
+  "frequency": 1,
+  "price": {
+    "enabled": true,
+    "price": 0
+  },
+  "task": {
+    "locale": "en",
+    "content": "{player_c} dance for {timer_10}"
+  }
+}'
+
+curl -X 'POST' \
+  'http://localhost:8082/task' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "type": "SINGLE",
+  "repeat": "ALWAYS",
+  "frequency": 1,
+  "price": {
+    "enabled": true,
+    "price": 0
+  },
+  "task": {
+    "locale": "en",
+    "content": "{player_c} look outside"
+  }
+}'
+
+curl -X 'POST' \
+  'http://localhost:8082/task' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "type": "SINGLE",
+  "repeat": "ALWAYS",
+  "frequency": 1,
+  "price": {
+    "enabled": true,
+    "price": 0
+  },
+  "task": {
+    "locale": "en",
+    "content": "{player_c} dance for {timer_10_3}"
+  }
+}'
+
+curl -X 'POST' \
+  'http://localhost:8082/task' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "type": "SINGLE",
+  "repeat": "ALWAYS",
+  "frequency": 1,
+  "price": {
+    "enabled": true,
+    "price": 0
+  },
+  "task": {
+    "locale": "en",
+    "content": "{player_c} sing for {timer_10_-1}"
+  }
+}'
+
+curl -X 'POST' \
+  'http://localhost:8082/task/3/sk' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{player_c} pozri sa von'
+
+curl -X 'POST' \
+  'http://localhost:8082/task/5/sk' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{player_c} spievaj {timer_10_-1}'
+
+curl -X 'POST' \
+  'http://localhost:8082/task' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "type": "DUO",
+  "repeat": "ALWAYS",
+  "frequency": 1,
+  "price": {
+    "enabled": true,
+    "price": 0
+  },
+  "task": {
+    "locale": "en",
+    "content": "Smile at each other for {timer_10_-1}"
+  }
+}'
+
+curl -X 'POST' \
+  'http://localhost:8082/task/6/sk' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d 'Usmievajte sa jeden na druheho na {timer_10_-1}'
