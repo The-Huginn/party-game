2806ccf5d45371dfe027ce55691cc90b80f4491e
The-Huginn <rbudinsk@redhat.com>
2023-09-10 12:58:13 +0200

    Update nginx proxy and frontend application urls

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,7 +1,8 @@
+import { dev } from '$app/environment';
 import { writable } from "svelte/store";
 
-export const game_url = 'http://game-service'
-export const task_url = 'http://task-game'
+export const game_url = dev ? 'http://localhost:8080' : 'https://game.thehuginn.com/api/game'
+export const task_url = dev ? 'http://localhost:8082' : 'https://game.thehuginn.com/api/task'
 export const header = writable({
     text: 'Party Game',
     append: ''
