bcf31636296e7094e90f581b6654b416a5a41813
The-Huginn <rbudinsk@redhat.com>
2023-09-10 10:49:48 +0200

    Containerize svelte app

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,7 +1,8 @@
-import { readable, writable } from "svelte/store";
+import { dev } from '$app/environment';
+import { writable } from "svelte/store";
 
-export const game_url = 'http://localhost:8080'
-export const task_url = 'http://localhost:8082'
+export const game_url = dev ? 'http://localhost:8080' : 'http://game-service'
+export const task_url = dev ? 'http://localhost:8082' : 'http://task-game'
 export const header = writable({
     text: 'Party Game',
     append: ''
