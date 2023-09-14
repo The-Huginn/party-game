5cd1357e300b6643e57f34a862be4dca803211c2
The-Huginn <rbudinsk@redhat.com>
2023-09-14 19:23:26 +0200

    Create population script for database

---
+++ population/populate.py
@@ -0,0 +1,29 @@
+import requests, json
+
+# api_url = "https://game.thehuginn.com/api/task/"
+api_url = "http://localhost:8082/"
+
+categories_file = open('categories.json', 'r')
+categories = json.loads(categories_file.read())
+
+tasks_file = open('tasks.json', 'r')
+tasks = json.loads(tasks_file.read())
+
+for category in categories:
+    response = requests.post(api_url + '/category', json=category['payload'])
+    id = response.json()['id']
+
+    for translation in category['translations']:
+        translation['category'] = id
+        requests.post(api_url + '/category/translation', json=translation)
+
+for task in tasks:
+    final_url = api_url + '/task'
+    if 'category' in task:
+        final_url = final_url + '/category/' + str(task['category'])
+
+    response = requests.post(final_url, json=task['payload'])
+    id = response.json()['id']
+
+    for translation in task['translations']:
+        requests.post(api_url + '/task/' + str(id) + '/' + translation['locale'], data=translation['translation'].encode())
