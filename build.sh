4af09a8685df16a7c3c186aca3875b6d7e0e6305
The-Huginn <rbudinsk@redhat.com>
2023-09-24 14:01:39 +0200

    Update build script to also synchronize tasks repo on server and automatically populate database

--- build.sh
+++ build.sh
@@ -4,18 +4,13 @@ git pull origin
 
 for microservice in game-service task-game
 do
-	cd ~/party-game/backend/$microservice&& \
+	cd ~/party-game/backend/$microservice && \
 	mvn clean install -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.resolver.transport=wagon -DskipTests && \
 	sed -i 's/thehuginn.com/localhost/g' target/kubernetes/kubernetes.yml && \
 	kubectl apply -f target/kubernetes/kubernetes.yml
 done
 
 kubectl rollout restart deployment svelte-frontend
-#podman login -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} registry.localhost
-#podman push registry.localhost/game:latest
 
-#kubectl scale --replicas=0 deploy/nginx
-#kubectl scale --replicas=0 deploy/backend
-
-#kubectl scale --replicas=1 deploy/backend
-#kubectl scale --replicas=1 deploy/nginx
+cd ~/party-game/population/tasks && git pull origin
+cd ~/party-game/population && python populate.py
