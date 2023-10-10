d2f667ed9eb994d40a83841326082ca2404fc50f
The-Huginn <rbudinsk@redhat.com>
2023-10-10 23:23:40 +0200

    Create static instancation method for PubTask

--- backend/assignment-game/src/main/java/com/thehuginn/task/PubTask.java
+++ backend/assignment-game/src/main/java/com/thehuginn/task/PubTask.java
@@ -1,6 +1,8 @@
 package com.thehuginn.task;
 
 import com.thehuginn.common.game.task.AbstractTask;
+import com.thehuginn.common.game.translation.LocaleTaskText;
+import com.thehuginn.common.game.translation.TaskText;
 import io.quarkus.panache.common.Sort;
 import io.smallrye.mutiny.Uni;
 import jakarta.persistence.DiscriminatorValue;
@@ -9,10 +11,32 @@ import jakarta.persistence.Entity;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 @DiscriminatorValue("1")
 public class PubTask extends AbstractTask {
+
+    public static Uni<PubTask> createPubTask(
+            String task,
+            Map<String, String> translations) {
+        PubTask pubTask = new PubTask();
+        pubTask.task = new TaskText(pubTask, "en", task);
+        List<Uni<LocaleTaskText>> localeTaskTexts = new ArrayList<>();
+        for (Map.Entry<String, String> translation : translations.entrySet()) {
+            localeTaskTexts
+                    .add(new LocaleTaskText(pubTask.task, translation.getKey(), translation.getValue()).persistAndFlush());
+        }
+
+        if (localeTaskTexts.isEmpty()) {
+            return pubTask.persistAndFlush();
+        }
+
+        return Uni.combine().all().unis(localeTaskTexts)
+                .usingConcurrencyOf(1)
+                .discardItems()
+                .replaceWith(pubTask.persistAndFlush());
+    }
 
     public static Uni<List<? extends AbstractTask>> generateTasks() {
         return PubTask.<PubTask> listAll(Sort.ascending("id"))
