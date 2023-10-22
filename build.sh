dbf8502730046e67f66fff2e0d48670789738aaa
The-Huginn <rbudinsk@redhat.com>
2023-10-22 17:56:11 +0200

    Update building script to pull tasks' repository

--- build.sh
+++ build.sh
@@ -22,11 +22,10 @@ done
 echo -e '\033[1mRolling out update for frontend...\033[0m'
 kubectl rollout restart deployment svelte-frontend
 
-rm -rf ~/party-game/population/tasks && mkdir ~/party-game/population/tasks
-cd ~/party-game/population/tasks && git clone https://The-Huginn:${GITHUB_TOKEN}@github.com/The-Huginn/party-game-tasks.git .
-
 echo -e '\033[1mWaiting for pods to activate for 20 seconds...\033[0m'
 sleep 20
 echo -e '\033[1mPopulating tasks...\033[0m'
+
+cd ~/party-game-tasks && git pull
 cp ~/party-game-tasks/task-mode/* ~/party-game/population/tasks/
 cd ~/party-game/population && python3.11 populate.py
